93f85a6b2b4af3afae3e88247b211f0f93b27779
Carsten Klein <21992319+akcarsten@users.noreply.github.com>
2018-12-13 23:20:35 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bitfinex',
-    version='1.0',
+    version='1.1',
     packages=['bitfinex'],
     url='https://github.com/akcarsten/bitfinex',
     license='',
