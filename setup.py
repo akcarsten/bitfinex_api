532551d1b721b649b15a9bd85351f7251a033234
Carsten Klein <21992319+akcarsten@users.noreply.github.com>
2019-02-09 20:51:21 +0100

    updated setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='bitfinex',
+    name='bitfinex-tencars',
     version='1.3',
     packages=['bitfinex'],
     url='https://github.com/akcarsten/bitfinex',
     license='',
     author='Carsten Klein',
     author_email='https://www.linkedin.com/in/carsten-klein/',
-    description='Bitfinex REST API client'
+    description='Bitfinex REST API client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
